f4ff1cf2b934e92e2fc213a4e84e96878f16996e
Vitalii <vitalii.luniev83@gmail.com>
2020-10-26 18:47:34 +0300

    Updates

---
+++ precache-manifest.2e4f8ef3ad39cf74bba879f9cdf200e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d046f75b46f4a1bd114b42ae5c93a4b9",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "ef6b2479a9da9f952b66",
+    "url": "/Covid-19-Tracker/static/css/main.94448d9b.chunk.css"
+  },
+  {
+    "revision": "e629387def648ccd6945",
+    "url": "/Covid-19-Tracker/static/js/2.cd104e37.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/Covid-19-Tracker/static/js/2.cd104e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6b2479a9da9f952b66",
+    "url": "/Covid-19-Tracker/static/js/main.5eb158f0.chunk.js"
+  },
+  {
+    "revision": "3f9bfa923e184b907d1a",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.f79461fb.js"
+  }
+]);
